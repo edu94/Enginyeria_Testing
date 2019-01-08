3c61b61755d959d3e533fbc693c5938781346112
edu94 <eduard.robinat@gmail.com>
2019-01-08 17:11:40 +0100

    Initial commit

---
+++ src/kiosk/VoteCounter.java
@@ -0,0 +1,20 @@
+package kiosk;
+
+import data.Party;
+import java.util.Set;
+
+public class VoteCounter {
+
+    ???
+    public VoteCounter(Set<Party> validParties) { ??? }
+    public void countParty(Party party) { ??? }
+    public void countNull() { ??? }
+    public void countBlank() { ??? }
+    public void scrutinize(Party party) { ??? }
+    public int getVotesFor(Party party) { ??? }
+    public int getNulls() { ??? }
+    public int getBlanks() { ??? }
+    public int getTotal() { ??? }
+}
+
+}
---
+++ src/kiosk/VotingKiosk.java
@@ -0,0 +1,16 @@
+package kiosk;
+
+import data.MailAddress;
+import  data.Party;
+import services.*;
+
+public class VotingKiosk {
+
+    ???
+    public VotingKiosk() { ??? }
+    public void setElectoralOrganism(ElectoralOrganism eO) { ??? }
+    public void setMailerService(MailerService mService){ ???}
+
+    public void vote(Party party) { ??? }
+    public void sendeReceipt(MailAddress address) { ??? }
+}
